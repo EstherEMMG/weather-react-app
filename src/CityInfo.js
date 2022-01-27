eccb3492ab5f3de445088f1d4dd3039cdb29c75a
EstherEMMG <96057903+EstherEMMG@users.noreply.github.com>
2022-01-27 12:02:57 +0100

    Added axios and State to get info of Api calls

--- src/CityInfo.js
+++ src/CityInfo.js
@@ -1,44 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function CityInfo() {
-  return (
-    <div className="Weather">
-      <div className="Form">
-        <form>
-          <input type="text" placeholder="Enter city name" />
-          <button type="button" className="btn btn-primary">
-            Search
-          </button>
-        </form>
-      </div>
-      <br />
-      <div className="container row">
-        <div className="card col-7">
-          <div className="card-body">
-            <h2>
-              <span className="maintemp"></span>
-              <sup className="degrees">
-                7 ºC | <a href="/">ºF</a>
-              </sup>
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                alt="Clear"
-              />
-            </h2>
-            <h6>Humidity: 12%</h6>
-            <h6>Wind: 5km/h</h6>
+  let [weatherData, setWeatherData] = useState({ ready: false });
+  function handleRequest(response) {
+    setWeatherData({
+      ready: true,
+      date: "Monday 20 12:02",
+      temperature: Math.round(response.data.main.temp),
+      wind: Math.round(response.data.wind.speed),
+      city: response.data.name,
+      humidity: Math.round(response.data.main.humidity),
+      description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="Form">
+          <form>
+            <input type="text" placeholder="Enter city name" />
+            <button type="button" className="btn btn-primary">
+              Search
+            </button>
+          </form>
+        </div>
+        <br />
+        <div className="container row">
+          <div className="card col-7">
+            <div className="card-body">
+              <h2>
+                <span className="temperature">{weatherData.temperature}</span>
+                <span className="unit">
+                  ºC | <a href="/">ºF</a>
+                </span>
+                <img src={weatherData.iconUrl} alt={weatherData.description} />
+              </h2>
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind}km/h</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-5">
+            <h1>{weatherData.city}</h1>
+            <h4>
+              <span>{weatherData.date}</span>
+            </h4>
+            <h4>
+              <span className="text-capitalize">{weatherData.description}</span>
+            </h4>
           </div>
         </div>
-        <div className="col-5">
-          <h1>Paris</h1>
-          <h4>
-            <span>Monday 20 12:02</span>
-          </h4>
-          <h4>
-            <span>Sunny with some clouds</span>
-          </h4>
-        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "ed8f1b521e1ef205ffa5fdddc628ab7e";
+    let city = "Madrid";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleRequest);
+    return "Loading...";
+  }
 }
--- src/Forecast.js
+++ src/Forecast.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Forecast() {
   return (
     <div className="container">
-      <div class="col-2">
-        <div class="card">
-          <div class="card-body">
-            <div class="weather-forecast-day">Mon</div>
+      <div className="col-2">
+        <div className="card">
+          <div className="card-body">
+            <div className="weather-forecast-day">Mon</div>
             <img
               src="http://openweathermap.org/img/wn/${
                     forecastDay.weather[0].icon
@@ -14,9 +14,9 @@ export default function Forecast() {
               alt=""
               width="42"
             />
-            <div class="weather-forecast-temp">
-              <span class="max-temp">12</span>
-              <span class="min-temp"> 2º</span>
+            <div className="weather-forecast-temp">
+              <span className="max-temp">12</span>
+              <span className="min-temp"> 2º</span>
             </div>
           </div>
         </div>
